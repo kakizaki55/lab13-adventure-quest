cd6232e4e0628ce3662b5ffacf5eb68e95dec053
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-26 14:24:05 -0700

    i just need to fill out my hard coded data and i can turn in for lab13a

--- quest/quest.js
+++ quest/quest.js
@@ -1,10 +1,10 @@
-console.log('your on the quest page!');
+// console.log('your on the quest page!');
 
 import { quests } from '../data/data.js';
 import { findById } from '../utiles.js';
 
 const questTitle = document.getElementById('quest-title');
-const questImg = document.getElementById('quest-image');
+// const questImg = document.getElementById('quest-image');
 const questDescription = document.getElementById('quest-description');
 const questChoices = document.getElementById('quest-choices');
 
@@ -14,15 +14,12 @@ const params = new URLSearchParams(window.location.search);
 const questId = params.get('id');
 
 const currentQuest = findById(quests, questId);
-console.log(currentQuest);
-console.log(currentQuest.choices);
 
 questTitle.textContent = currentQuest.title;
 //need to replace img later 
 questDescription.textContent = currentQuest.description;
 
 for (let choice of currentQuest.choices){
-    console.log(choice);
     const div = document.createElement('div');
     div.textContent = choice.description;
     const radioButton = document.createElement('input');
