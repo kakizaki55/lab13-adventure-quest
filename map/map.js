80e04122adab47c055fd33e0714bf5b2abf66479
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-26 13:18:04 -0700

    got some anchortags working using the Url parametors, next i need to use the info to render the page on the quest page

--- map/map.js
+++ map/map.js
@@ -1,7 +1,16 @@
 import { quests } from '../data/data.js';
 import { creatQuestLink } from '../utiles.js';
 
-console.log(quests);
-console.log(creatQuestLink(quests));
+const mapElement = document.getElementById('map-links');
+
+
+for (let quest of quests) {
+    console.log(quest.id);
+    
+    const questLink = creatQuestLink(quest);
+
+    mapElement.append(questLink);
+
+}
 //NEED TO ADD PLACEMENT OF ANCHORTAG AND APPEND TO THE HTML ELEMENT.
 
--- utiles.js
+++ utiles.js
@@ -17,9 +17,12 @@ export function setPlayer(playerAnt){
     localStorage.setItem('PLAYER', stringyObj);
 }
 export function creatQuestLink(questId) {
+
     const anchorTag = document.createElement('a');
+    anchorTag.classList.add('quest');
     anchorTag.href = `../quest/?id=${questId.id}`;
     anchorTag.textContent = questId.title;
+    
 
     return anchorTag;
 }
