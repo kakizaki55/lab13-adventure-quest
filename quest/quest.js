5b55b7709ed528ab93b9b9280d5bc17fac3c1da4
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-26 14:02:14 -0700

    got the rendering of the radio buttons to work

--- quest/quest.js
+++ quest/quest.js
@@ -14,3 +14,23 @@ const params = new URLSearchParams(window.location.search);
 const questId = params.get('id');
 
 const currentQuest = findById(quests, questId);
+console.log(currentQuest);
+console.log(currentQuest.choices);
+
+questTitle.textContent = currentQuest.title;
+//need to replace img later 
+questDescription.textContent = currentQuest.description;
+
+for (let choice of currentQuest.choices){
+    console.log(choice);
+    const div = document.createElement('div');
+    div.textContent = choice.description;
+    const radioButton = document.createElement('input');
+    radioButton.type = 'radio';
+    radioButton.value = choice.id;
+    radioButton.name = 'choice';
+
+    div.append(radioButton);
+    questChoices.append(div);
+}
+
